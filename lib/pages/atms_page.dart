38cb2998012bb943b80c5a92e424fde16d3cb354
Obeng Emmanuel <eobengjunior@gmail.com>
2023-03-04 23:28:29 +0000

    Pages Updated

---
+++ lib/pages/atms_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ATMs extends StatefulWidget {
+  const ATMs({super.key});
+
+  @override
+  State<ATMs> createState() => _ATMsState();
+}
+
+class _ATMsState extends State<ATMs> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
